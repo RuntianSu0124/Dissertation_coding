bd7ca5b6bbee08d6292544aed0afc121e3032c34
RuntianSu0124 <pmxrs6@nottingham.ac.uk>
2022-07-08 00:37:11 +0100

    1o times CV

--- MachineLearningMethod.R
+++ MachineLearningMethod.R
@@ -1,5 +1,6 @@
 library(pls)
 library(glmnet)
+set.seed(123)
 ##store the yeast genotype data in a matrix
 yeast_geno<-matrix(NA,nrow=1008,ncol=11623)
 int1<-1
@@ -11,11 +12,14 @@ for(i in 1:16){
 }
 
 
+
+
 ##Ridge regression:Alpha=0
 lambdas<-10^seq(3,-4,by=-.1)
 missings<-c(NA,length=46)
-R2_ridge<-c(NA,length=46)
+R2_ridge<-matrix(NA,nrow=46,ncol=10)
 for(i in 1:46){
+for(k in 1:10){
 Y_raw<-c(cross$pheno[[i]])
 nn<-c(which(Y_raw>-1000))## Exclusion of missing values
 missings[i]<-1008-length(nn)
@@ -28,20 +32,21 @@ X.train<-X[num,]
 Y.test<-Y[-num]
 X.test<-X[-num,]
 #train the model by using train set 
-cv_fit<-cv.glmnet(X.train,Y.train,alpha=0,lambda=lambdas,nfolds=10)#10 fold Cross-validation
+cv_fit<-cv.glmnet(X.train,Y.train,alpha=0,lambda=lambdas,nfolds=10)# Cross-validation
 lambda<-cv_fit$lambda.min #the optimal lambda
 RR_model<-glmnet(X.train,Y.train,alpha=0, lambda=lambda)
 #calculate R-square
 y_bar<-mean(Y.test)
 predict_y<-predict(RR_model,s=lambda,newx=X.test)
-R2_ridge[i]<-1-sum((predict_y-y_bar)^2)/sum((Y.test-y_bar)^2)
-}
-R2_ridge
+R2_ridge[i,k]<-1-sum((predict_y-y_bar)^2)/sum((Y.test-y_bar)^2)
+}}
+
 
 
 ##Lasso regression:Alpha=1
-R2_lasso<-c(NA,length=46)
+R2_lasso<-matrix(NA,nrow=46,ncol=10)
 for(i in 1:46){
+  for(k in 1:10){
   Y_raw<-c(cross$pheno[[i]])
   nn<-c(which(Y_raw>-1000))## Exclusion of missing values
   Y<-Y_raw[nn] #pheotype data without missing values
@@ -53,21 +58,23 @@ for(i in 1:46){
   Y.test<-Y[-num]
   X.test<-X[-num,]
   #train the model by using train set 
-  cv_fit<-cv.glmnet(X.train,Y.train,alpha=1,lambda=lambdas,nfolds=10)#10 fold Cross-validation
+  cv_fit<-cv.glmnet(X.train,Y.train,alpha=1,lambda=lambdas,nfolds=10)#Cross-validation
   lambda<-cv_fit$lambda.min #the optimal lambda
   LA_model<-glmnet(X.train,Y.train,alpha=1, lambda=lambda)
   #calculate R-square on test set
   y_bar<-mean(Y.test)
   predict_y<-predict(LA_model,s=lambda,newx=X.test)
-  R2_lasso[i]<-1-sum((predict_y-y_bar)^2)/sum((Y.test-y_bar)^2)
-}
+  R2_lasso[i,k]<-1-sum((predict_y-y_bar)^2)/sum((Y.test-y_bar)^2)
+}}
 R2_lasso
 
 
 ##Elastic net: Alpha between 0 to 1
 Alphas<-seq(0,1,by=0.05)
-R2_elastic<-c(NA,length=46)
-alphas<-c(NA,length=46)
+R2_elastic<-matrix(NA,nrow=46,ncol=10)
+alphas<-matrix(NA,nrow=46,ncol=10)
+
+for(g in 1:10){
 for(i in 1:46){
   R2_Alphas<-c(NA,length=21)
   for(k in 1:21){
@@ -81,8 +88,8 @@ for(i in 1:46){
   X.train<-X[num,]
   Y.test<-Y[-num]
   X.test<-X[-num,]
-  #
-  cv_fit<-cv.glmnet(X.train,Y.train,alpha=Alphas[k],lambda=lambdas,nfolds=10)#10 fold Cross-validation
+  #CV
+  cv_fit<-cv.glmnet(X.train,Y.train,alpha=Alphas[k],lambda=lambdas,nfolds=10)#Cross-validation
   lambda<-cv_fit$lambda.min #the optimal lambda
   EN_model<-glmnet(X,Y,alpha=Alphas[k],lambda=lambda)
   #calculate R-square
@@ -90,22 +97,53 @@ for(i in 1:46){
   predict_y<-predict(EN_model,s=lambda,newx=X.test)
   R2_Alphas[k]<-1-sum((predict_y-y_bar)^2)/sum((Y.test-y_bar)^2)
   }
-  alphas[i]<-which.max(R2_Alphas)
-  R2_elastic[i]<-max(R2_Alphas)
-}
-Alphas[alphas]# the value of alpha
+  alphas[i,g]<-which.max(R2_Alphas)
+  R2_elastic[i,g]<-max(R2_Alphas)
+}}
+Alphas[alphas]# the values of alpha, 需要修改
 R2_elastic
 
 
 ## Principal component regression:PCR
-
-R2_PCR<-c(NA,length=46)
+npcs<-matrix(NA,nrow=46,ncol=10)#record the number of PCs 
+R2_PCR<-matrix(NA,nrow=46,ncol=10)
+for(g in 1:10){
 for(i in 1:46){
-  
-  
-  
-}
-PCR_model<-pcr(Y.train~X.train,scale=FALSE,validation="CV")
+  Y_raw<-c(cross$pheno[[i]])
+  nn<-c(which(Y_raw>-1000))## Exclusion of missing values
+  Y<-Y_raw[nn] #pheotype data without missing values
+  X<-yeast_geno[nn,]
+  #Dividing the training and test set
+  num<-sample(length(Y),0.8*length(Y))
+  Y.train<-Y[num]
+  X.train<-X[num,]
+  Y.test<-Y[-num]
+  X.test<-X[-num,]
+  #train the model by using train set 
+  PCR_model<-pcr(Y.train~X.train,scale=FALSE,validation="CV",segments=10)# Cross Validation
+  cverr<-RMSEP(PCR_model)$val[1,,]
+  npcs[i,g]<-which.min(cverr)-1
+  predict_y<-predict(PCR_model,X.test,ncomp=npcs[i])
+  y_bar<-mean(Y.test)
+  R2_PCR[i,g]<-1-sum((predict_y-y_bar)^2)/sum((Y.test-y_bar)^2)
+}}
+PCR_model<-pcr(Y.train~X.train,scale=FALSE,validation="CV",segments=10)
+validationplot(PCR_model,val.type="MSEP") #使用 validatìonplot ()函数作出交叉验证得分的图像，MSE是均方误差
+plot(RMSEP(PCR_model), legendpos = "topright")
+plot(PCR_model, method="onesigma")
+?pcr
+predict_y<-predict(PCR_model,X.test,ncomp=110)
+y_bar<-mean(Y.test)
+1-sum((predict_y-y_bar)^2)/sum((Y.test-y_bar)^2)
+cverr<-RMSEP(PCR_model)$val[1,,]
+npcs<-which.min(cverr)-1
+
+##Random Forest
 
 
+
+
+
+##
+R2s<-cbind(R2_ridge,R2_lasso,R2_elastic,R2_PCR)
 
